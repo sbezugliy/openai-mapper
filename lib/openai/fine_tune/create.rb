050f11e0237ddb7974dbe6cba5cdf51dd7d52833
Sergey Bezugliy <s.bezugliy@gmail.com>
2023-10-22 14:54:11 +0300

    Images

---
+++ lib/openai/fine_tune/create.rb
@@ -0,0 +1,12 @@
+module Openai
+  module FineTune
+    class Create < ::Openai::Resource::ApiResource
+      def initialize
+      end
+
+      def request(request)
+
+      end
+    end
+  end
+end
